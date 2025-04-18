3eea859b2f81ffd444af41cd062955ccd0553784
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2025-04-18 09:31:25 +0300

    commit in main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { DayOfWeek, isWeekend } from "./basic/5";
-import { createUserData, mango, poly } from "./basic/6";
-import { page1, page2 } from "./basic/7";
-import "./style.css";
+// import { DayOfWeek, isWeekend } from "./basic/5";
+// import { createUserData, mango, poly } from "./basic/6";
+// import { page1, page2 } from "./basic/7";
+// import "./style.css";
 
 // console.log("Sunday", isWeekend(DayOfWeek.Sunday));
 // console.log(createUserData(poly));
